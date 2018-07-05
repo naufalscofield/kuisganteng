efb3c74fdeaa544d3aac8162953b1ffbf07278ab
Dadihasanudin <superuduy@gmail.com>
2018-07-05 21:03:04 +0700

    Dadi2

--- app.py
+++ app.py
@@ -787,7 +787,7 @@ def elektronik():
         'Barang elektronik':'Kipas angin'
     },
     {
-        'Barang elektronik':'Speaker'
+        'Barang elektronik':'Speakerr'
     }
     ]
     return jsonify({'Barang elektronik':elektronik})
