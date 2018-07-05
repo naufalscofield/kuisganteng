c7fc86bc24edd8ce613cecfa3f4f4bb2913c172c
naufalscofield <poisonjoker6@gmail.com>
2018-07-05 11:14:40 +0700

    naufal

--- app.py
+++ app.py
@@ -9,62 +9,16 @@ def hello_world(gurih):
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
-
-@app.route('/crot', methods=['POST'])
-def login():
-	return request.form['anu']
-
-
-@app.route('/input/alamat')
+#
+# @app.route('/crot', methods=['POST'])
+# def login():
+# 	return request.form['anu']
+@app.route('/input/mahasiswa')
 def alamat():
     alamat = [
     {
-        'kodepos':40121,
-        'kelurahan':'cicadas'
-    },
-    {
-        'kodepos':40611,
-        'kelurahan':'cigending'
-    },
-    {
-        'kodepos':63463,
-        'kelurahan':'plancungan'
-    },
-    {
-        'kodepos' :20745,
-        'kelurahan' :'damai'
-    },
-    {
-        'kodepos' :34124,
-        'kelurahan' :'tejosari'
-    },
-    {
-        'kodepos' :40617,
-        'kelurahan' :'Pasanggrahan'
-    },
-    {
-        'kodepos' :20744,
-        'kelurahan' :'kebun lada'
-    },
-    {
-        'kodepos' :34112,
-        'kelurahan' :'tejo agung'
-    },
-    {
-        'kodepos' :63471,
-        'kelurahan' :'mangunsuman'
-    },
-    {
-        'kodepos' :34117,
-        'kelurahan' :'banjar sari'
-    },
-    {
-        'kodepos' :20743,
-        'kelurahan' :'pahlawan'
-    },
-    {
-        'kodepos' :40619,
-        'kelurahan' :'Pasir Endah'
+        'npm':1164001,
+        'nama':'naufal'
     }
 ]
     return jsonify({'daftar alamat':alamat})  #will return the json
